e303b1e6c6069f2b633f3ba1527f46c7bb88df69
xavier-romero <xavier@iden3.com>
2022-05-23 11:06:23 +0200

    postgres big test

--- postgres/app/app.py
+++ postgres/app/app.py
@@ -77,7 +77,7 @@ for step in steps:
 
     delta = time.time() - t
     print(f'RETRIEVAL OF {len(ks)} TOOK {delta:.3f}s '
-        f'({int(step/delta)} op/s)', flush=True)
+          f'({int(step/delta)} op/s)', flush=True)
 
     # DELETE KEYS AND VALUES
     t = time.time()
@@ -89,7 +89,7 @@ for step in steps:
 
     delta = time.time() - t
     print(f'REMOVAL OF {len(ks)} TOOK {delta:.3f}s '
-        f'({int(step/delta)} op/s)', flush=True)
+          f'({int(step/delta)} op/s)', flush=True)
 
     # END
     print(f':: END STEP {step}')
